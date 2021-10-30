4a9bf8c74957e488490fc5d00316ce0b81453ac7
Loi Nguyen <thangloi2501@gmail.com>
2021-10-30 23:45:56 +0800

    initial commit

---
+++ src/main/java/loi/socket/SocketTextHandler.java
@@ -0,0 +1,45 @@
+package loi.socket;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SocketTextHandler extends TextWebSocketHandler {
+
+	private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		sessions.add(session);
+		super.afterConnectionEstablished(session);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		sessions.remove(session);
+		super.afterConnectionClosed(session, status);
+	}
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		super.handleTextMessage(session, message);
+		sessions.forEach(webSocketSession -> {
+			try {
+				String payload = message.getPayload();
+				JSONObject jsonObject = new JSONObject(payload);
+				webSocketSession.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+
+			} catch (IOException e) {
+			}
+		});
+	}
+
+}
